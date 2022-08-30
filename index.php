c48d352f1a26003ec88232f028166ae36f41b61a
navitha-shankar <112075488+navitha-shankar@users.noreply.github.com>
2022-08-30 16:17:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,33 +5,33 @@
 </head>
 <body>  
 <center>       
-<h1>Storing Form data in Database</h1> 
-<formaction="insert.php"method="post">            
+<h1> Storing Form data in Database</h1> 
+<form action="insert.php"method="post">            
 <p>            
-<labelfor="firstName">FirstName:</label>       
-<inputtype="text"name="first_name"id="firstName">          
+<label for="firstName">FirstName:</label>       
+<input type="text"name="first_name"id="firstName">          
 </p>        
 
 <p>         
-<labelfor="lastName">Last Name:</label>          
-<inputtype="text"name="last_name"id="lastName">     
+<label for="lastName">Last Name:</label>          
+<input type="text"name="last_name"id="lastName">     
 </p>            
 
 <p>           
-<labelfor="Gender">Gender:</label>        
-<inputtype="text"name="gender"id="Gender">         
+<label for="Gender">Gender:</label>        
+<input type="text"name="gender"id="Gender">         
 </p>        
 
 <p>         
-<labelfor="Address">Address:</label>       
-<inputtype="text"name="address"id="Address">     
+<label for="Address">Address:</label>       
+<input type="text"name="address"id="Address">     
 </p>   
 
 <p>           
-<labelfor="emailAddress">EmailAddress:</label>        
-<inputtype="text"name="email"id="emailAddress">           
+<label for="emailAddress">EmailAddress:</label>        
+<input type="text"name="email"id="emailAddress">           
 </p>         
-<inputtype="submit"value="Submit">     
+<input type="submit"value="Submit">     
 </form>   
 </center>
 </body>
